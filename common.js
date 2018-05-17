34f4f7d0d21e7b0e12c77acb61eb817aa7e5f503
Andrew <xhm1031@gmail.com>
2018-05-16 21:37:08 -0400

    error

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $("#footer-social-google").click(function() {
-        $(".footer-social-modal-google")modal("show");
+        $(".footer-social-modal-google").modal("show");
     })
 })
