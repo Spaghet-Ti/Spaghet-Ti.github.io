43e2a1a21c23ae1a1812e0e0cb8621f63ab23531
Andrew <xhm1031@gmail.com>
2018-05-19 17:19:46 -0400

    update

--- common.js
+++ common.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
-    $(".footer-social-google").click(function() {
+    $("#header-navbar-collapse").click(function() {
+        if($("#header-navbar-collapse-menu").css("width") == 0){
+            $("#header-navbar-collapse-menu").css("width", "255px")
+        }else{
+            $("#header-navbar-collapse-menu").css("width", "0")
+        }
+    })
+    $("#footer-social-google").click(function() {
         $(".footer-social-modal-google").modal("show");
     })
-    $(".footer-social-wechat").click(function() {
+    $("#footer-social-wechat").click(function() {
         $(".footer-social-modal-wechat").modal("show");
     })
-    $(".footer-social-email").click(function() {
+    $("#footer-social-email").click(function() {
         $(".footer-social-modal-email").modal("show");
     })
 })
