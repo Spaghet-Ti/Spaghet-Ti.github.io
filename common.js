85f2d50db436329baa714fd8364dca52a89f0a42
Andrew <xhm1031@gmail.com>
2018-05-20 15:27:31 -0400

    the ml-auto should only be applied on small screens

--- common.js
+++ common.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-    $("#header-navbar-collapse").click(function() {
-        if($("#header-navbar-collapse-menu").css("width") == 0){
-            $("#header-navbar-collapse-menu").css("width", "255px")
-        }else{
-            $("#header-navbar-collapse-menu").css("width", "0")
-        }
-    })
     $("#footer-social-google").click(function() {
         $(".footer-social-modal-google").modal("show");
     })
